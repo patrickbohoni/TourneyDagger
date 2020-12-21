1de7cc4fc476fe150c9e628bb188508c982d1ee0
Patrick Bohoni <patrick.bohoni1114@gmail.com>
2020-12-21 18:32:52 +0200

    Initial upload - create Maven Project

---
+++ src/main/java/com/tourneydagger/main/TourneyDagger/TourneyDaggerApplication.java
@@ -0,0 +1,13 @@
+package com.tourneydagger.main.TourneyDagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourneyDaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourneyDaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourneydagger/main/TourneyDagger/TourneyDaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourneydagger.main.TourneyDagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourneyDaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
